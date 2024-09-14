8347025e20b839faa9e074a33586b3d308a6f0d8
shweta <shwetasinghh9416@gmail.com>
2024-09-14 23:47:46 +0530

    tailwind added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <div>
-        i am shweta
-      </div>
+      <h1 className="text-3xl bg-red-100 font-bold underline">Hello world!</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
